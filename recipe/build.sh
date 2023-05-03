6b8b03447c3dd014a63c984df5a21790adf1c9ac
adibbley <adibbley@nvidia.com>
2023-05-03 13:42:16 -0400

    Update cudaroot in pc files

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@
 [[ -d lib64 ]] && mv lib64 lib
 mkdir -p ${PREFIX}/lib/stubs
 [[ -d pkg-config ]] && mv pkg-config ${PREFIX}/lib/pkgconfig
-[[ -d "$PREFIX/lib/pkgconfig" ]] && sed -E -i "s|cudaroot=.+|cudaroot=$PREFIX|g" $PREFIX/lib/pkgconfig/cuda*.pc
+[[ -d "$PREFIX/lib/pkgconfig" ]] && sed -E -i "s|cudaroot=.+|cudaroot=$PREFIX/cuda|g" $PREFIX/lib/pkgconfig/cuda*.pc
 
 [[ ${target_platform} == "linux-64" ]] && targetsDir="targets/x86_64-linux"
 [[ ${target_platform} == "linux-ppc64le" ]] && targetsDir="targets/ppc64le-linux"
