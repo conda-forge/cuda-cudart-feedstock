19e71ba042303ba29f0403adddf5bd882f3b18f1
adibbley <adibbley@nvidia.com>
2023-05-03 12:08:43 -0400

    Moving everything to $PREFIX/cuda

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,24 +10,10 @@ mkdir -p ${PREFIX}/lib/stubs
 [[ ${target_platform} == "linux-ppc64le" ]] && targetsDir="targets/ppc64le-linux"
 [[ ${target_platform} == "linux-aarch64" ]] && targetsDir="targets/sbsa-linux"
 
-for i in `ls`; do
-    [[ $i == "build_env_setup.sh" ]] && continue
-    [[ $i == "conda_build.sh" ]] && continue
-    [[ $i == "metadata_conda_debug.yaml" ]] && continue
-    if [[ $i == "lib" ]] || [[ $i == "include" ]]; then
-        # Headers and libraries are installed to targetsDir
-        mkdir -p ${PREFIX}/${targetsDir}
-        mkdir -p ${PREFIX}/$i
-        cp -rv $i ${PREFIX}/${targetsDir}
-        if [[ $i == "lib" ]]; then
-            for j in "$i"/{*.a,*.so,stubs/*.so}*; do
-                # Shared and static libraries are symlinked in $PREFIX/lib
-                ln -sv ${PREFIX}/${targetsDir}/$j ${PREFIX}/$j
-            done
-        fi
-    else
-        # Put all other files in targetsDir
-        mkdir -p ${PREFIX}/${targetsDir}/${PKG_NAME}
-        cp -rv $i ${PREFIX}/${targetsDir}/${PKG_NAME}/$i
-    fi
-done
+mkdir -p ${PREFIX}/cuda/${targetsDir}
+
+cp -rv lib ${PREFIX}/cuda/${targetsDir}
+cp -rv include ${PREFIX}/cuda/${targetsDir}
+
+ln -sv ${PREFIX}/cuda/${targetsDir}/lib ${PREFIX}/cuda/lib64
+ln -sv ${PREFIX}/cuda/${targetsDir}/include ${PREFIX}/cuda/include
