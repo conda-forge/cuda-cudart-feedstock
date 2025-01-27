19c7b5ca9b1258ac241531b3959d070a34016fc3
Daniel Ching <dching@nvidia.com>
2025-01-27 10:00:38 -0600

    BLD: Match tegra target directory with upstream naming convention

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,8 +8,14 @@ mkdir -p ${PREFIX}/lib/stubs
 
 [[ ${target_platform} == "linux-64" ]] && targetsDir="targets/x86_64-linux"
 [[ ${target_platform} == "linux-ppc64le" ]] && targetsDir="targets/ppc64le-linux"
+# https://docs.nvidia.com/cuda/cuda-compiler-driver-nvcc/index.html?highlight=tegra#cross-compilation
 [[ ${target_platform} == "linux-aarch64" && ${arm_variant_type} == "sbsa" ]] && targetsDir="targets/sbsa-linux"
-[[ ${target_platform} == "linux-aarch64" && ${arm_variant_type} == "tegra" ]] && targetsDir="targets/tegra-linux"
+[[ ${target_platform} == "linux-aarch64" && ${arm_variant_type} == "tegra" ]] && targetsDir="targets/aarch64-linux"
+
+if [ -z "${targetsDir+x}" ]; then
+    echo "target_platform: ${target_platform} is unknown! targetsDir must be defined!" >&2
+    exit 1
+fi
 
 for i in `ls`; do
     [[ $i == "build_env_setup.sh" ]] && continue
